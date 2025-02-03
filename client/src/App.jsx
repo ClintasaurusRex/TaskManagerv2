ecafda218af785c1b43c459dd001b41abee80937
Clint <clinthiles1992@gmail.com>
2025-02-03 14:37:58 -0800

    Remove default Vite React client project files

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>New task manager</h1>
+      <div className='all-tasks'>
+        <Task />
+        <Task />
+        <Task />
+      </div>
+    </>
+  );
+}
+
+function Task() {
+  return (
+    <>
+      <main className='main-container'>
+        <div className='tasks-title'>Tasks</div>
+        <ul className='list-items'>
+          <li>task</li>
+          <li>task</li>
+          <li>task</li>
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default App;
